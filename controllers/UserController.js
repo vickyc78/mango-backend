89ee2c846e77e3f8de96e0106f2939dade73e37d
vickywohlig <vicky.chotaliya@wohlig.com>
2020-08-09 12:53:00 +0530

    cors added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,54 +1,55 @@
-let UserModel = require("../models/UserModel");
+let UserModel = require("../models/UserModel")
 
-module.exports = function(router) {
-  //   router.post("/createUser", (req, res, next) => {
-  //     // console.log("req.body req.body", req.body);
-  //     UserModel.createUser(req.body, (err, saveResponse) => {
-  //       //   console.log("saveResponse saveResponse", saveResponse);
-  //       if (err) {
-  //         res.status(200).json({ err: "Failed to Save" });
-  //       } else {
-  //         res.status(200).json(saveResponse);
-  //       }
-  //     });
-  //   });
-  // router.post("/createUser", async (req, res, next) => {
-  //   try {
-  //     res.status(200).json(await UserModel.createUser(req.body));
-  //   } catch (err) {
-  //     res.status(200).json(err);
-  //   }
-  // });
+module.exports = function (router) {
+	//   router.post("/createUser", (req, res, next) => {
+	//     // console.log("req.body req.body", req.body);
+	//     UserModel.createUser(req.body, (err, saveResponse) => {
+	//       //   console.log("saveResponse saveResponse", saveResponse);
+	//       if (err) {
+	//         res.status(200).json({ err: "Failed to Save" });
+	//       } else {
+	//         res.status(200).json(saveResponse);
+	//       }
+	//     });
+	//   });
+	// router.post("/createUser", async (req, res, next) => {
+	//   try {
+	//     res.status(200).json(await UserModel.createUser(req.body));
+	//   } catch (err) {
+	//     res.status(200).json(err);
+	//   }
+	// });
 
-  router.post("/createUser", (req, res, next) => {
-    UserModel.createUser(req.body, function(err, user) {
-      if (err) {
-        res.json({
-          error: err
-        });
-      } else {
-        res.send(user);
-      }
-    });
-  });
+	router.post("/createUser", (req, res, next) => {
+		console.log("createUsercreateUser", req.body)
+		UserModel.createUser(req.body, function (err, user) {
+			if (err) {
+				res.json({
+					error: err,
+				})
+			} else {
+				res.send(user)
+			}
+		})
+	})
 
-  router.get("/verifyOtp", async (req, res, next) => {
-    try {
-      res.status(200).json(await UserModel.verifyOtp(req.body));
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+	router.get("/verifyOtp", async (req, res, next) => {
+		try {
+			res.status(200).json(await UserModel.verifyOtp(req.body))
+		} catch (error) {
+			res.status(500).json(error)
+		}
+	})
 
-  router.get("/loginUser", (req, res, next) => {
-    UserModel.loginUser(req.body, (err, login) => {
-      if (err) {
-        res.json({
-          err: err
-        });
-      } else {
-        res.json(login);
-      }
-    });
-  });
-};
+	router.get("/loginUser", (req, res, next) => {
+		UserModel.loginUser(req.body, (err, login) => {
+			if (err) {
+				res.json({
+					err: err,
+				})
+			} else {
+				res.json(login)
+			}
+		})
+	})
+}
--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-let express = require("express");
+let express = require("express")
 
-let mongoose = require("mongoose");
+let mongoose = require("mongoose")
 
-let bodyParser = require("body-parser");
+let bodyParser = require("body-parser")
 
-let Schema = mongoose.Schema;
+let cors = require("cors")
 
-let router = express.Router();
+let Schema = mongoose.Schema
 
-let app = express();
+let router = express.Router()
 
-var env = require("./config/env/development");
+let app = express()
+
+var env = require("./config/env/development")
 
 mongoose.connect(env.dbUrl, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+})
+app.use(cors())
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-let port = process.env.PORT || env.port;
-app.listen(port, function() {
-  console.log(`App listing on port ${port} !`);
-});
+let port = process.env.PORT || env.port
+app.listen(port, function () {
+	console.log(`App listing on port ${port} !`)
+})
 
-let userRoutes = require("./controllers/UserController");
-let productRoutes = require("./controllers/ProductController");
-let orderRoutes = require("./controllers/OrderController");
+let userRoutes = require("./controllers/UserController")
+let productRoutes = require("./controllers/ProductController")
+let orderRoutes = require("./controllers/OrderController")
 
-app.use("/User", router);
-app.use("/Product", router);
-app.use("/Order", router);
+app.use("/User", router)
+app.use("/Product", router)
+app.use("/Order", router)
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-  );
-  next();
-});
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Credentials", "true")
+	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With")
+	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+	)
+	next()
+})
 
-userRoutes(router);
-productRoutes(router);
-orderRoutes(router);
+userRoutes(router)
+productRoutes(router)
+orderRoutes(router)
 
-app.get("/", function(req, res) {
-  res.send("Hello Express");
-});
+app.get("/", function (req, res) {
+	res.send("Hello Express")
+})
 
 app.get("/vicky", (req, res) => {
-  res.status(200).send("Hello Express");
-  // res.status(204).json("Hello Express");
-});
+	res.status(200).send("Hello Express")
+	// res.status(204).json("Hello Express");
+})
