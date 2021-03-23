df35e0dac9f72136ec0a7351f4d5e5fc34a6ec1f
Glitch (blue-crimson-asparagus) <none>
2021-03-23 19:38:20 +0000

    😬🚜 Checkpoint ./index.js:29945205/54

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/vicky", (req, res) => {
-  res.status(200).send("Hello Express");
+  res.status(200).send("Hello kapil");
   // res.status(204).json("Hello Express");
 });
 app.get("/", (request, response) => {
