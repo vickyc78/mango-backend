5c13292cab183c6c22e2713337be9ddf83436919
vickywohlig <vicky.chotaliya@wohlig.com>
2021-03-24 00:41:51 +0530

    glitch

--- index.js
+++ index.js
@@ -1,62 +1,72 @@
-let express = require("express")
+let express = require("express");
 
-let mongoose = require("mongoose")
+let mongoose = require("mongoose");
 
-let bodyParser = require("body-parser")
+let bodyParser = require("body-parser");
 
-let cors = require("cors")
+let cors = require("cors");
 
-let Schema = mongoose.Schema
+let Schema = mongoose.Schema;
 
-let router = express.Router()
+let router = express.Router();
 
-let app = express()
-
-var env = require("./config/env/development")
+let app = express();
+var env;
+env = require("./config/env/development");
+// env = require("./config/env/production");
+console.log("process.env", env);
 
 mongoose.connect(env.dbUrl, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-})
-app.use(cors())
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+app.use(cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let port = process.env.PORT || env.port
-app.listen(port, function () {
-	console.log(`App listing on port ${port} !`)
-})
+let port = process.env.PORT || env.port;
+app.listen(port, function() {
+  console.log(`App listing on port ${port} !`);
+});
 
-let userRoutes = require("./controllers/UserController")
-let productRoutes = require("./controllers/ProductController")
-let orderRoutes = require("./controllers/OrderController")
+let userRoutes = require("./controllers/UserController");
+let productRoutes = require("./controllers/ProductController");
+let orderRoutes = require("./controllers/OrderController");
 
-app.use("/User", router)
-app.use("/Product", router)
-app.use("/Order", router)
+app.use("/User", router);
+app.use("/Product", router);
+app.use("/Order", router);
 
-app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*")
-	res.setHeader("Access-Control-Allow-Credentials", "true")
-	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With")
-	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-	)
-	next()
-})
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+  );
+  next();
+});
 
-userRoutes(router)
-productRoutes(router)
-orderRoutes(router)
+userRoutes(router);
+productRoutes(router);
+orderRoutes(router);
 
-app.get("/", function (req, res) {
-	res.send("Hello Express")
-})
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
 app.get("/vicky", (req, res) => {
-	res.status(200).send("Hello Express")
-	// res.status(204).json("Hello Express");
-})
+  res.status(200).send("Hello Express");
+  // res.status(204).json("Hello Express");
+});
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://spiced-jasper-tugboat.glitch.me/`);
+}, 280000);
