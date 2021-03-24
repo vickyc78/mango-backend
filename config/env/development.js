b65463c1938eb988a6482876135df55235b37ce1
Glitch (blue-crimson-asparagus) <none>
2021-03-24 19:44:47 +0000

    ✋🍦 Checkpoint ./config/env/development.js:29945205/1301

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: 3000,
-  dbUrl: "mongodb://admin@cluster0-shard-00-00.amer3.mongodb.net:27017,cluster0-shard-00-01.amer3.mongodb.net:27017,cluster0-shard-00-02.amer3.mongodb.net:27017/mango?3t.databases=mango&3t.uriVersion=2&3t.certificatePreference=RootCACert:accept_any&3t.connection.name=mumbai-mango&3t.useClientCertPassword=false&readPreference=primary&ssl=true&3t.sharded=true&3t.connectionMode=multi&replicaSet=true",
+  dbUrl: "mongodb://admin:admin@cluster0-shard-00-00.amer3.mongodb.net:27017,cluster0-shard-00-01.amer3.mongodb.net:27017,cluster0-shard-00-02.amer3.mongodb.net:27017/mangodb?ssl=true&replicaSet=atlas-drxcqg-shard-0&authSource=admin&retryWrites=true&w=majority",
   name: "development"
 };
